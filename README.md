53e99f7056019e38372d7fc76e7e36103f307dcc
Lucas Bardeli de Bastiani Silva <lucas.silva1111@fatec.sp.gov.br>
2025-07-06 20:55:43 -0300

    Criando as outras rotas de produtos

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
 
-## API:
+## API com Python e Flask:
 API completa que simula um sistema de e-commerce, com rotas, integração com um banco de dados, 
 autenticação de usuário e funcionalidades essenciais, como listar produtos, adicionar itens ao 
 carrinho de compras e realizar o checkout.
 
-## Rocketseat:
-Códigos desenvolvidos no curso de Python com Flask - Curso Introdutório da Rocketseat.
-```bash
-  https://app.rocketseat.com.br/journey/python-com-flask-curso-introdutorio/overview
-```
-
-## Como executar:
-* Você pode baixar os aquivos através do botão **Add file** acima dos arquivos do repositório e depois selecionar o **Upload files**.
-* Ou se preferir, no terminal do computador você pode clonar esse repositório digitando o comando:
-```bash
-  git clone https://github.com/lucas-bardeli/api-python-flask.git
-```
-
-## Python:
-* É preciso ter a linguagem instalada no computador:
-```bash
-  https://www.python.org/downloads/
-```
-* Preferencialmente, instale as extensões de Python no VS Code;
 * Para baixar as bibliotecas necessárias digite no terminal:
 ```bash
-  pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 ```
-* Instale a extensão SQLite Viewer para poder abrir o banco de dados;
+* Instale a extensão SQLite Viewer do VS Code para poder visualizar o banco de dados.
--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 # Importação
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 # Modelagem:
 # Produto (id, name, price, description)
@@ -22,7 +24,7 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
-    if (('name' in data) and ('price' in data)): 
+    if (("name" in data) and ("price" in data)): 
         product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
         db.session.add(product)
         db.session.commit()
@@ -45,10 +47,56 @@ def delete_product(product_id):
     return jsonify({"message": "Product not found"}), 404
 
 
+# Recuperar produto
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+
+# Atualizar produto
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    data = request.json
+    if ("name" in data):
+        product.name = data["name"]
+    if ("price" in data):
+        product.price = data["price"]
+    if ("description" in data):
+        product.description = data["description"]
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"})
+
+
+# Recuperar produtos
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)
+
+
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
-@app.route('/hello')
+@app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
